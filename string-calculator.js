2f226df370bb61f72764b2d2c48f2d820dd464e0
Anshuman-Programmer <173656519+anshuman-boas@users.noreply.github.com>
2025-01-12 04:18:09 +0530

    final code format

--- string-calculator.js
+++ string-calculator.js
@@ -1,23 +1,27 @@
 function add(numbers) {
-    if (numbers === "") return 0;
-  
-    let delimiter = ",";
-    if (numbers.startsWith("//")) {
-      const delimiterMatch = numbers.match(/^\/\/(.+)\n/);
-      if (delimiterMatch) {
-        delimiter = delimiterMatch[1];
-        numbers = numbers.slice(delimiterMatch[0].length);
-      }
+  if (numbers === "") return 0;
+
+  let delimiter = ",";
+  if (numbers.startsWith("//")) {
+    const delimiterMatch = numbers.match(/^\/\/(.+)\n/);
+    if (delimiterMatch) {
+      delimiter = delimiterMatch[1];
+      numbers = numbers.slice(delimiterMatch[0].length);
     }
-  
-    const nums = numbers.replace(/\n/g, delimiter).split(delimiter).map(num => parseInt(num, 10)).filter(num => !isNaN(num));
-  
-    // Check for negatives
-    const negatives = nums.filter(num => num < 0);
-    if (negatives.length > 0) {
-      throw new Error(`negative numbers not allowed: ${negatives.join(",")}`);
-    }
-  
-    return nums.reduce((sum, num) => sum + num, 0);
   }
+
+  const nums = numbers
+    .replace(/\n/g, delimiter)
+    .split(delimiter)
+    .map((num) => parseInt(num, 10))
+    .filter((num) => !isNaN(num));
+
+  const negatives = nums.filter((num) => num < 0);
+  if (negatives.length > 0) {
+    throw new Error(`negative numbers not allowed: ${negatives.join(",")}`);
+  }
+
+  return nums.reduce((sum, num) => sum + num, 0);
+}
+
 module.exports = { add };
