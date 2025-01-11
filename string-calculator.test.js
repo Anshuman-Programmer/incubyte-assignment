434bd43268e1d9aa11f570c4642194341a861add
Anshuman-Programmer <173656519+anshuman-boas@users.noreply.github.com>
2025-01-12 04:10:22 +0530

    Handle a Single Number

--- string-calculator.test.js
+++ string-calculator.test.js
@@ -4,4 +4,10 @@ describe('String Calculator', () => {
   test('should return 0 for an empty string', () => {
     expect(add("")).toBe(0);
   });
+  
+  test('should return the number itself if the string contains one number', () => {
+    expect(add("1")).toBe(1);
+  });
+  
 });
+
