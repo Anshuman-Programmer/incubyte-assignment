9b2a21e48bf5fdbf23f2681f9592b5ece64f79f8
Anshuman-Programmer <173656519+anshuman-boas@users.noreply.github.com>
2025-01-12 04:11:35 +0530

    Handle Two Numbers

--- string-calculator.test.js
+++ string-calculator.test.js
@@ -4,9 +4,13 @@ describe('String Calculator', () => {
   test('should return 0 for an empty string', () => {
     expect(add("")).toBe(0);
   });
-  
+
   test('should return the number itself if the string contains one number', () => {
     expect(add("1")).toBe(1);
+  });
+  
+  test('should return the sum of two numbers', () => {
+    expect(add("1,5")).toBe(6);
   });
   
 });
